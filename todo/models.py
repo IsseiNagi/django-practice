8654b55d4b17c04d9e3629b65540a6491d23e5ec
IsseiNagi <issei.nagi@gmail.com>
2021-06-29 12:08:16 +0900

    update models.py and migrations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class TodoModel(models.Model):
+    # カラムとフィールドデータ型を指定
+    title = models.CharField(max_length=100)  # CharFiledはmax_lengthの指定が必須
+    memo = models.TextField()  # CharFieldより多い文字数を入力できるフィールド
